31fdf15fb9baa4ccbc3bb144062492742d83c1c8
antoniusfaro <antoniusf44@gmail.com>
2023-11-06 02:49:02 +0700

    config: typescripts

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ApolloProvider from "./providers/ApolloProvider";
+import ContactProvider from "./providers/ContactProvider";
+import NotificationProvider from "./providers/NotificationProvider";
+import RouterProvider from "./providers/RouterProvider";
+import "./styles/variables.css"
+
+function App() {
+  return (
+    <ApolloProvider>
+      <ContactProvider>
+        <NotificationProvider>
+          <RouterProvider />
+        </NotificationProvider>
+      </ContactProvider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
